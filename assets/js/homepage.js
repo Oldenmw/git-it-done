656b244f6d575e2fa9cb0cb39822b5ad9b17ea7e
Matt Oldenburg <theoldenmw@gmail.com>
2021-08-24 14:21:33 -0400

    fetch api

---
+++ assets/js/homepage.js
@@ -0,0 +1,11 @@
+var getUserRepos = function(user) {
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos("microsoft");
